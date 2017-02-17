7ae1dc104eb9caa78a65f9ac692933e7129432bf
Auriles El Haddad <auriles0404@gmail.com>
2017-02-17 20:38:44 +0100

    create apple

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onload = function() {
   var ctx;
   var delay = 100;
   var snakee;
+  var applee;
 
   init();
 
@@ -17,6 +18,7 @@ window.onload = function() {
     document.body.appendChild(canvas);
     ctx = canvas.getContext("2d");
     snakee = new Snake([[6,4],[5,4],[4,4]], "right");
+    applee = new Apple([10,10]);
     refreshCanvas();
   }
 
@@ -24,6 +26,7 @@ window.onload = function() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     snakee.moove();
     snakee.draw();
+    applee.draw();
     setTimeout(refreshCanvas,delay);
   }
 
@@ -82,6 +85,21 @@ window.onload = function() {
       if(allowedDirections.indexOf(newDirection) > -1) {
         this.direction = newDirection;
       }
+    };
+  }
+
+  function Apple(position) {
+    this.position = position;
+    this.draw = function() {
+      ctx.save();
+      ctx.fillStyle = "#33cc33";
+      ctx.beginPath();
+      var radius = blockSize/2;
+      var x = position[0]*blockSize + radius;
+      var y = position[1]*blockSize + radius;
+      ctx.arc(x, y, radius, 0, Math.PI*2, true);
+      ctx.fill();
+      ctx.restore();
     };
   }
 
