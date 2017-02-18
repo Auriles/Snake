ad126633b14b3a642087bba129b6bdaabfe590d6
Auriles El Haddad <auriles0404@gmail.com>
2017-02-18 13:54:51 +0100

    wall collision

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ window.onload = function() {
   var canvasHeight = 600;
   var blockSize = 30;
   var ctx;
-  var delay = 100;
+  var delay = 1000;
   var snakee;
   var applee;
+  var widthInBlocks = canvasWidth/blockSize;
+  var heightInBlocks = canvasHeight/blockSize;
 
   init();
 
@@ -17,17 +19,21 @@ window.onload = function() {
     canvas.style.border = "1px solid";
     document.body.appendChild(canvas);
     ctx = canvas.getContext("2d");
-    snakee = new Snake([[6,4],[5,4],[4,4]], "right");
+    snakee = new Snake([[6,4],[5,4],[4,4],[3,4],[2,4]], "right");
     applee = new Apple([10,10]);
     refreshCanvas();
   }
 
   function refreshCanvas() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     snakee.moove();
-    snakee.draw();
-    applee.draw();
-    setTimeout(refreshCanvas,delay);
+    if(snakee.checkCollision()) {
+      // game over
+    } else {
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+      snakee.draw();
+      applee.draw();
+      setTimeout(refreshCanvas,delay);
+    }
   }
 
   function drawBlock(ctx, position) {
@@ -85,6 +91,31 @@ window.onload = function() {
       if(allowedDirections.indexOf(newDirection) > -1) {
         this.direction = newDirection;
       }
+    };
+    this.checkCollision = function() {
+      var wallCollision = false;
+      var snakeCollision = false;
+      var head = this.body[0];
+      var rest = this.body.slice(1);
+      var snakeX = head[0];
+      var snakeY = head[1];
+      var minX = 0;
+      var minY = 0;
+      var maxX = widthInBlocks - 1;
+      var maxY = heightInBlocks - 1;
+      var isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
+      var isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
+
+      if(isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls) {
+        wallCollision = true;
+      }
+
+      for(var i = 0; i < rest.length; i++) {
+        if(snakeX === rest[i][0] && snakeY === rest[i][1]) {
+          snakeCollision = true;
+        }
+      }
+      return wallCollision || snakeCollision;
     };
   }
 
